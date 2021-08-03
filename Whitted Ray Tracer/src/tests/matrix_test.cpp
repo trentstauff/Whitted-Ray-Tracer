3693333a4649513c78780fc05d62cdc6996fcd1a
Trent <53923200+trentstauff@users.noreply.github.com>
2021-08-02 23:21:20 -0400

    Inverse tests

--- Whitted Ray Tracer/src/tests/matrix_test.cpp
+++ Whitted Ray Tracer/src/tests/matrix_test.cpp
@@ -246,7 +246,7 @@ namespace {
 
 	}
 
-	TEST(MatrixTest, Inverse) {
+	TEST(MatrixTest, Inverse1) {
 
 		auto a = Matrix(4, 4);
 		double arr[] = { -5, 2, 6, -8, 1, -5, 1, 8, 7, 7, -6, -7, 1, -3, 7, 4 };
@@ -260,6 +260,56 @@ namespace {
 
 		EXPECT_TRUE(result == expected);
 		
+	}
+
+	TEST(MatrixTest, Inverse2) {
+
+		auto a = Matrix(4, 4);
+		double arr[] = { 8, -5, 9, 2, 7, 5, 6, 1, -6, 0, 9, 6, -3, 0, -9, -4 };
+		a.set(arr);
+
+		auto result = a.inverse();
+
+		auto expected = Matrix(4, 4);
+		double arr2[] = { -0.15385, -0.15385, -0.28205, -0.53846, -0.07692, 0.12308, 0.02564, 0.03077, 0.35897, 0.35897, 0.43590, 0.92308, -0.69231, -0.69231, -0.76923, -1.92308 };
+		expected.set(arr2);
+		
+		EXPECT_TRUE(result == expected);
+
+	}
+
+	TEST(MatrixTest, Inverse3) {
+
+		auto a = Matrix(4, 4);
+		double arr[] = { 9, 3, 0, 9, -5, -2, -6, -3, -4, 9, 6, 4, -7, 6, 6, 2 };
+		a.set(arr);
+
+		auto result = a.inverse();
+
+		auto expected = Matrix(4, 4);
+		double arr2[] = { -0.04074, -0.07778, 0.14444, -0.22222, -0.07778, 0.03333, 0.36667, -0.33333, -0.02901, -0.14630, -0.10926, 0.12963, 0.17778, 0.06667, -0.26667, 0.33333 };
+		expected.set(arr2);
+
+		EXPECT_TRUE(result == expected);
+
+	}
+
+	TEST(MatrixTest, MultProductByInverse) {
+
+		auto a = Matrix(4, 4);
+		double arr[] = { 3, -9, 7, 3, 3, -8, 2, -9, -4, 4, 4, 1, -6, 5, -1, 1 };
+		a.set(arr);
+
+		auto b = Matrix(4, 4);
+		double arr2[] = { 8, 2, 2, 2, 3, -1, 7, 0, 7, 0, 5, 4, 6, -2, 0, 5 };
+		b.set(arr2);
+
+		auto c = a * b;
+
+		auto d = c * b.inverse();
+		
+		EXPECT_TRUE(a == d);
+
 	}
 	
 }
